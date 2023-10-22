9c596d28b82737bf2187b457e8241891e7810611
Daniel Lucrédio <dlucredio@gmail.com>
2023-10-22 12:58:55 -0300

    Antes de testar no server Cabana

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import datetime
+import gspread
 import discord
 from discord.ext import tasks
+from discord.ext import commands
 import yaml
-from utils import enviar_mensagem_com_reacao
+from utils import enviar_mensagem_com_reacao, ler_planilha_interativos, postar_interativo
 import logging
 
 # # Configurando logging
@@ -14,52 +16,125 @@ with open('config.yml', 'r', encoding='utf-8') as file:
 with open('token.yml', 'r', encoding='utf-8') as file:
     config_token = yaml.safe_load(file)
 
+canal_comandos = int(config['canal_comandos'])
+
 # Configuração do cliente Discord
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 # Vamos configurar a tarefa da mensagem recorrente
-@tasks.loop(time=[datetime.time(hour=16), 
+@tasks.loop(time=[datetime.time(hour=0),
+                  datetime.time(hour=1),
+                  datetime.time(hour=2),
+                  datetime.time(hour=3),
+                  datetime.time(hour=4),
+                  datetime.time(hour=5),
+                  datetime.time(hour=6),
+                  datetime.time(hour=7),
+                  datetime.time(hour=8),
+                  datetime.time(hour=9),
+                  datetime.time(hour=10),
+                  datetime.time(hour=11),
+                  datetime.time(hour=12),
+                  datetime.time(hour=13), 
+                  datetime.time(hour=14), 
+                  datetime.time(hour=15), 
+                  datetime.time(hour=16), 
                   datetime.time(hour=17),
                   datetime.time(hour=18),
                   datetime.time(hour=19),
                   datetime.time(hour=20),
                   datetime.time(hour=21),
                   datetime.time(hour=22),
-                  datetime.time(hour=23),
-                  datetime.time(hour=0),
-                  datetime.time(hour=1),
-                  datetime.time(hour=2),
-                  datetime.time(hour=3)
-                  ])
-async def mensagemProgramada():
+                  datetime.time(hour=23)])
+async def mensagems_programadas():
+    config_msg = config['mensagens_programadas']
+    diferenca_zonahoraria_utc = int(config_msg['diferenca_zonahoraria_utc'])
+
     now = datetime.datetime.now()
-    current_hour_brazil = now.hour - 3
-    if current_hour_brazil < 1:
-        current_hour_brazil = current_hour_brazil + 12
-    indice = current_hour_brazil - 12
+    hora_local = now.hour + diferenca_zonahoraria_utc
 
-    texto = config['textos']['tumba'][indice]
+    chave_mensagem = f'texto_{hora_local}h'
 
-    await enviar_mensagem_com_reacao(client, config['canais']['taverna'], texto, config['emojis']['caveira'])
+    if not chave_mensagem in config_msg:
+        logging.info(f'Nenhuma mensagem configurada: {chave_mensagem}')
+    else:
+        logging.info(f'Postando mensagem configurada: {chave_mensagem}')
+        texto = config_msg[chave_mensagem]
 
-@client.event
+        await enviar_mensagem_com_reacao(bot, config_msg['canal'], texto, config_msg['emoji_reacao'])
+
+@bot.event
 async def on_ready():
-    logging.info(f'Bot logado como: {client.user}')
-    if not mensagemProgramada.is_running():
-        mensagemProgramada.start()
-        logging.info('Mensagem programada configurada!')
+    logging.info(f'Bot logado como: {bot.user}')
+    if not mensagems_programadas.is_running():
+        mensagems_programadas.start()
+        logging.info('Mensagem de hora em hora configurada!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.author.name == 'daniel.lucredio':
-        if message.content.startswith('Oi, Bruxa'):
-            await enviar_mensagem_com_reacao(client, config['canais']['taverna'], "Olá, mestre!", config['emojis']['caveira'])
-        if message.content.startswith('Bruxa, qual é o seu nome?'):
-            await enviar_mensagem_com_reacao(client, config['canais']['taverna'], "Meu nome é Hécate, ó grande mestre dos magos!", config['emojis']['caveira'])
+@bot.command()
+async def ping(ctx):
+    if pode_executar_comando(ctx):
+        await ctx.send(f'pong')
 
-client.run(config_token['token_bot'], root_logger=True)
+@bot.command()
+async def publicar_interativo(ctx):
+    if pode_executar_comando(ctx):
+        now = datetime.datetime.now()
+        dia = now.day
+        mes = now.month
+        try:
+            await ctx.send(f'Publicando {dia}/{mes}, certo! Só um minuto...')
+            interativos_programados=config['interativos_programados']
+            gc = gspread.service_account(filename=interativos_programados['arquivo_credenciais'])
+            planilha = gc.open_by_key(interativos_programados['id_planilha'])
+            dados_postagem = ler_planilha_interativos(planilha=planilha, dia=dia, mes=mes)
+            canal_postagem = dados_postagem['canal']
+            await postar_interativo(bot=bot, canal=canal_postagem, dados_postagem=dados_postagem)
+            await ctx.send(f'Prontinho!')
+        except Exception as e:
+            if 'WorksheetNotFound' in str(e):
+                await ctx.send(f'Nada a postar em {dia}/{mes}')
+            elif 'index' in str(e) and 'not found' in str(e):
+                await ctx.send(f'Nada a postar em {dia}/{mes}')
+            else:
+                await ctx.send(f'{e}')
+            logging.error(e)
+            raise
+
+@bot.command()
+async def preview_interativo(ctx, diaMes):
+    if pode_executar_comando(ctx):
+        try:
+            args = diaMes.split('/')
+            dia = int(args[0])
+            mes = int(args[1])
+        except Exception as e1:
+            await ctx.send(f'O jeito certo de usar é $preview_interativo dia/mês')
+            print(e1)
+            raise
+        try:
+            await ctx.send(f'Preview de {dia}/{mes}, certo! Só um minuto...')
+            interativos_programados=config['interativos_programados']
+            gc = gspread.service_account(filename=interativos_programados['arquivo_credenciais'])
+            planilha = gc.open_by_key(interativos_programados['id_planilha'])
+            dados_postagem = ler_planilha_interativos(planilha=planilha, dia=dia, mes=mes)
+            canal_postagem = interativos_programados['canal_preview_mensagens']
+            if dados_postagem['tipo'].startswith("t"):
+                canal_postagem = interativos_programados['canal_preview_forum']
+            await postar_interativo(bot=bot, canal=canal_postagem, dados_postagem=dados_postagem)
+            await ctx.send(f'Prontinho!')
+        except Exception as e:
+            if 'WorksheetNotFound' in str(e):
+                await ctx.send(f'Nada a postar em {dia}/{mes}')
+            elif 'index' in str(e) and 'not found' in str(e):
+                await ctx.send(f'Nada a postar em {dia}/{mes}')
+            else:
+                await ctx.send(f'{e}')
+            logging.error(e)
+            raise
+
+def pode_executar_comando(ctx):
+    return ctx.channel.id == canal_comandos
+
+bot.run(config_token['token_bot'], root_logger=True)
--- utils.py
+++ utils.py
@@ -1,9 +1,63 @@
+import discord
 import logging
 
-async def enviar_mensagem_com_reacao(client, canal, texto, reacao):
-    channel = client.get_channel(canal)
+async def enviar_mensagem_com_reacao(bot, canal, texto, reacao):
+    channel = bot.get_channel(canal)
     if not channel:
-        logging.error(f'Erro ao enviar mensagem "{texto}": canal {canal} não encontrado')
+        raise Exception(f'Erro ao enviar mensagem "{texto}": canal {canal} não encontrado')
     else:
         mensagem = await channel.send(texto)
         await mensagem.add_reaction(reacao)
+
+def criar_embed(cor, titulo, cargo, descricao, url_imagem, rodape):
+    embed = discord.Embed(color=cor, title=titulo, description="<@&"+cargo+">\n\n"+descricao)
+    embed.set_image(url=url_imagem)
+    embed.set_footer(text=rodape)
+    return embed
+
+async def enviar_mensagem_embed(bot, canal, embed):
+    channel = bot.get_channel(canal)
+    if not channel:
+        raise Exception(f'Erro ao enviar mensagem com embed: canal {canal} não encontrado')
+    else:
+        await channel.send(embed=embed)
+
+async def criar_thread_embed(bot, canal, embed, nome_thread, conteudo_thread):
+    channel = bot.get_channel(canal)
+    if not channel:
+        raise Exception(f'Erro ao criar thread com embed: fórum {canal} não encontrado')
+    else:
+        await channel.create_thread(name=nome_thread,embed=embed,content=conteudo_thread)
+
+def ler_planilha_interativos(planilha, dia, mes):
+    logging.info(f'Lendo planilha...')
+    worksheet = planilha.get_worksheet(mes-1)
+    row = dia+1
+
+    logging.info(f'Planilha lida')
+
+    tipoPostagem = worksheet.cell(row,2).value
+    if not tipoPostagem:
+        raise Exception(f'Nada a postar em {dia}/{mes}')
+    else:
+        return {
+            "tipo": worksheet.cell(row,2).value,
+            "canal": int(worksheet.cell(row,3).value),
+            "cargo": worksheet.cell(row,4).value,
+            "nome_thread": worksheet.cell(row,5).value,
+            "conteudo_thread": worksheet.cell(row,6).value,
+            "titulo": worksheet.cell(row,7).value,
+            "descricao": worksheet.cell(row,8).value,
+            "rodape": worksheet.cell(row,9).value,
+            "url_imagem": worksheet.cell(row,10).value,
+            "cor": int("0x"+worksheet.cell(row,11).value,16)
+        }
+
+async def postar_interativo(bot, canal, dados_postagem):
+    logging.info(f'Criando postagem: {dados_postagem}')
+
+    embed=criar_embed(cor=dados_postagem['cor'], titulo=dados_postagem['titulo'], cargo=dados_postagem['cargo'], descricao=dados_postagem['descricao'], url_imagem=dados_postagem['url_imagem'], rodape=dados_postagem['rodape'])
+    if dados_postagem['tipo'].startswith("m"):
+        await enviar_mensagem_embed(bot=bot, canal=canal, embed=embed)
+    elif dados_postagem['tipo'].startswith("t"):
+        await criar_thread_embed(bot=bot, canal=canal, nome_thread=dados_postagem['nome_thread'], conteudo_thread=dados_postagem['conteudo_thread'], embed=embed)
